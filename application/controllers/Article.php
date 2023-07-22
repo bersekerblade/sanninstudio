f0b11093e2e27d678987cf252cd0054bf2a6cb7c
Faiz <hairrichtalk@gmail.com>
2023-07-23 02:06:47 +0700

    Update button add article

--- application/controllers/Article.php
+++ application/controllers/Article.php
@@ -13,11 +13,17 @@ class Article extends CI_Controller
     public function index()
     {
         $data['title'] = 'Article';
-
         $data['tbl_article'] = $this->Article_model->getAllArticle();
-
         $this->load->view('templates/header', $data);
         $this->load->view('article/index', $data);
+        $this->load->view('templates/footer');
+    }
+
+    public function add()
+    {
+        $data['title'] = 'Add Article';
+        $this->load->view('templates/header', $data);
+        $this->load->view('article/add');
         $this->load->view('templates/footer');
     }
 }
