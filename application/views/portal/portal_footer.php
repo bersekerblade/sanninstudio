cb8cd2b794cfcdf1522d7e648db477cd0b14ff11
Faiz <hairrichtalk@gmail.com>
2023-12-03 03:13:22 +0700

    togle menu portal

--- application/views/portal/portal_footer.php
+++ application/views/portal/portal_footer.php
@@ -1,26 +1,16 @@
 <!-- footer start -->
 <footer class="footer-global">
     <div class="container-fluid">
-        <p class="desc"><a href="https://jkt48.com/?lang=id">TOP</a> | <a href="https://jkt48.com/news/list?lang=id">News</a> | <a href="https://jkt48.com/member/list?lang=id">Member</a> | <a href="https://jkt48.com/about/jkt48?lang=id">Apa Itu JKT48</a> | <a href="https://jkt48.com/about/akb48?lang=id">Apa Itu AKB48</a> | <a href="https://jkt48.com/about/producer?lang=id">Pengenalan Produser</a> | <a href="mailto:info@jkt48.com">Kontak</a> | <a href="https://jkt48.com/rules?lang=id">Syarat dan Ketentuan</a></p>
-        <p class="sns"><a href="https://www.facebook.com/official.JKT48" target="_blank"><svg class="svg-inline--fa fa-facebook-square fa-w-14" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="facebook-square" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" data-fa-i2svg="">
-                    <path fill="currentColor" d="M400 32H48A48 48 0 0 0 0 80v352a48 48 0 0 0 48 48h137.25V327.69h-63V256h63v-54.64c0-62.15 37-96.48 93.67-96.48 27.14 0 55.52 4.84 55.52 4.84v61h-31.27c-30.81 0-40.42 19.12-40.42 38.73V256h68.78l-11 71.69h-57.78V480H400a48 48 0 0 0 48-48V80a48 48 0 0 0-48-48z"></path>
-                </svg><!-- <i class="fab fa-facebook-square"></i> Font Awesome fontawesome.com --></a> | <a href="https://twitter.com/officialjkt48" target="_blank"><svg class="svg-inline--fa fa-twitter-square fa-w-14" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="twitter-square" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" data-fa-i2svg="">
-                    <path fill="currentColor" d="M400 32H48C21.5 32 0 53.5 0 80v352c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V80c0-26.5-21.5-48-48-48zm-48.9 158.8c.2 2.8.2 5.7.2 8.5 0 86.7-66 186.6-186.6 186.6-37.2 0-71.7-10.8-100.7-29.4 5.3.6 10.4.8 15.8.8 30.7 0 58.9-10.4 81.4-28-28.8-.6-53-19.5-61.3-45.5 10.1 1.5 19.2 1.5 29.6-1.2-30-6.1-52.5-32.5-52.5-64.4v-.8c8.7 4.9 18.9 7.9 29.6 8.3a65.447 65.447 0 0 1-29.2-54.6c0-12.2 3.2-23.4 8.9-33.1 32.3 39.8 80.8 65.8 135.2 68.6-9.3-44.5 24-80.6 64-80.6 18.9 0 35.9 7.9 47.9 20.7 14.8-2.8 29-8.3 41.6-15.8-4.9 15.2-15.2 28-28.8 36.1 13.2-1.4 26-5.1 37.8-10.2-8.9 13.1-20.1 24.7-32.9 34z"></path>
-                </svg><!-- <i class="fab fa-twitter-square"></i> Font Awesome fontawesome.com --></a> | <a href="https://www.instagram.com/jkt48" target="_blank"><svg class="svg-inline--fa fa-instagram fa-w-14" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="instagram" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" data-fa-i2svg="">
-                    <path fill="currentColor" d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"></path>
-                </svg><!-- <i class="fab fa-instagram"></i> Font Awesome fontawesome.com --></a> | <a href="https://www.youtube.com/user/JKT48" target="_blank"><svg class="svg-inline--fa fa-youtube-square fa-w-14" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="youtube-square" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" data-fa-i2svg="">
-                    <path fill="currentColor" d="M186.8 202.1l95.2 54.1-95.2 54.1V202.1zM448 80v352c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V80c0-26.5 21.5-48 48-48h352c26.5 0 48 21.5 48 48zm-42 176.3s0-59.6-7.6-88.2c-4.2-15.8-16.5-28.2-32.2-32.4C337.9 128 224 128 224 128s-113.9 0-142.2 7.7c-15.7 4.2-28 16.6-32.2 32.4-7.6 28.5-7.6 88.2-7.6 88.2s0 59.6 7.6 88.2c4.2 15.8 16.5 27.7 32.2 31.9C110.1 384 224 384 224 384s113.9 0 142.2-7.7c15.7-4.2 28-16.1 32.2-31.9 7.6-28.5 7.6-88.1 7.6-88.1z"></path>
-                </svg><!-- <i class="fab fa-youtube-square"></i> Font Awesome fontawesome.com --></a> | <a href="https://www.tiktok.com/@jkt48.official" target="_blank"><svg class="svg-inline--fa fa-tiktok fa-w-14" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="tiktok" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" data-fa-i2svg="">
-                    <path fill="currentColor" d="M448,209.91a210.06,210.06,0,0,1-122.77-39.25V349.38A162.55,162.55,0,1,1,185,188.31V278.2a74.62,74.62,0,1,0,52.23,71.18V0l88,0a121.18,121.18,0,0,0,1.86,22.17h0A122.18,122.18,0,0,0,381,102.39a121.43,121.43,0,0,0,67,20.14Z"></path>
-                </svg><!-- <i class="fab fa-tiktok"></i> Font Awesome fontawesome.com --></a></p>
-        <p>Copyright © 2023 JKT48 Operation Team (PT. Indonesia Musik Nusantara) All Rights Reserved</p>
+        <p>Copyright © <?= date('Y'); ?> SANNIN STUDIO All Rights Reserved</p>
     </div>
 </footer>
+
+
 <div class="sidebar main left" id="sidebar-left" data-sidebar-left="closed" style="position: fixed; top: 0px; bottom: 0px; width: 375px; z-index: 3000; left: -375px;">
     <div class="sidebar__wrapper">
         <div class="d-flex justify-content-between align-items-center p-3 bg-primary text-white">
             <div>
-                <a id="sidebar-close" href="https://jkt48.com/news/list?lang=id#"><svg class="svg-inline--fa fa-times fa-w-11 text-white" aria-hidden="true" focusable="false" data-prefix="fa" data-icon="times" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 352 512" data-fa-i2svg="">
+                <a id="sidebar-close" href="#"><svg class="svg-inline--fa fa-times fa-w-11 text-white" aria-hidden="true" focusable="false" data-prefix="fa" data-icon="times" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 352 512" data-fa-i2svg="">
                         <path fill="currentColor" d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z"></path>
                     </svg><!-- <i class="fa fa-times text-white" aria-hidden="true"></i> Font Awesome fontawesome.com --></a>
             </div>
@@ -77,30 +67,8 @@
         </div>
     </div>
 </div>
-<script type="text/javascript" async="" src="./JKT48 _ Berita Terbaru_files/analytics.js.download"></script>
-<script type="text/javascript" async="" src="./JKT48 _ Berita Terbaru_files/js"></script>
-<script src="./JKT48 _ Berita Terbaru_files/app.min.js.download"></script>
-<script src="./JKT48 _ Berita Terbaru_files/my-script.min.js.download"></script>
-<script async="" src="./JKT48 _ Berita Terbaru_files/js(1)"></script>
-<script>
-    window.dataLayer = window.dataLayer || [];
-
-    function gtag() {
-        dataLayer.push(arguments);
-    }
-    gtag('js', new Date());
-    gtag('config', 'UA-26218343-1');
-</script>
-<script type="application/ld+json">
-    {
-        "@context": "http://schema.org",
-        "@type": "Organization",
-        "name": "JKT48 Official Web Site",
-        "url": "https://jkt48.com/",
-        "sameAs": ["https://www.facebook.com/official.JKT48", "https://twitter.com/officialjkt48", "https://www.instagram.com/jkt48", "https://www.youtube.com/user/JKT48", "https://www.tiktok.com/@jkt48.official"]
-    }
-</script>
-
+<script src="<?= base_url('assets/portal/'); ?>js/app.min.js"></script>
+<script src="<?= base_url('assets/portal/'); ?>js/my-script.min.js"></script>
 
 
 <div data-sidebar-left="mask" style="position: fixed; inset: 0px; z-index: 2999; display: none; background-color: rgb(0, 0, 0); opacity: 0.5;"></div>
--- application/views/portal/portal_header.php
+++ application/views/portal/portal_header.php
@@ -6,8 +6,8 @@
 
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
-    <meta name="keywords" content="JKT48,JKT,Jakarta,indonesia,AKB48,SKE48,NMB48,HKT48,NGT48,STU48,BNK48,MNL48,SGO48,CGM48">
-    <meta name="author" content="JKT48 Operation Team">
+    <meta name="keywords" content="SANNIN,Studio,Kota Batu">
+    <meta name="author" content="Sannin Studio Operation Team">
     <title>Sannin Studio</title>
 
     <!-- Favicon -->
@@ -29,48 +29,37 @@
                 </div>
                 <div class="header-global__pc--menu">
                     <div class="header-global__pc--menu__user">
-                        <a href="https://jkt48.com/login?lang=id">Login</a> / <a href="https://jkt48.com/register?lang=id">Register</a>
+                        <a href="<?= base_url('auth'); ?>">Login</a> / <a href="<?= base_url('auth/registration'); ?>">Register</a>
                     </div>
                 </div>
             </div>
         </div>
+        <!-- menu toogle start  -->
+        <div class="header-global__sp">
+            <div class="container-fluid">
+                <div class="header-global__sp--content">
+                    <h1><a href="#">SANNIN STUDIO</a></h1>
+                    <div class="header-global__sp--content__hamburger">
+                        <a id="sidebar-left-trigger" href="#"><svg class="svg-inline--fa fa-bars fa-w-14" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="bars" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" data-fa-i2svg="">
+                                <path fill="currentColor" d="M16 132h416c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z"></path>
+                            </svg><!-- <i class="fas fa-bars"></i> Font Awesome fontawesome.com --></a>
+                    </div>
+                    <div class="header-global__sp--content__menu">
+                        <p><a href="#">Login</a> | <a href="#">Register</a></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- menu toogle end  -->
     </div>
     <!-- main container start  -->
     <nav class="nav-global">
         <div class="container">
             <ul class="nav-global__list d-flex align-items-center justify-content-between">
-                <li class="nav-global__item"><a href="https://jkt48.com/news/list?lang=id">NEWS</a></li>
-                <li class="nav-global__item"><a href="https://jkt48.com/calendar/list?lang=id">SCHEDULE</a></li>
-                <li class="nav-global__item"><a href="https://jkt48.com/member/list?lang=id">MEMBER</a></li>
-                <li class="nav-global__item">THEATER <svg class="svg-inline--fa fa-chevron-down fa-w-14" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="chevron-down" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" data-fa-i2svg="">
-                        <path fill="currentColor" d="M207.029 381.476L12.686 187.132c-9.373-9.373-9.373-24.569 0-33.941l22.667-22.667c9.357-9.357 24.522-9.375 33.901-.04L224 284.505l154.745-154.021c9.379-9.335 24.544-9.317 33.901.04l22.667 22.667c9.373 9.373 9.373 24.569 0 33.941L240.971 381.476c-9.373 9.372-24.569 9.372-33.942 0z"></path>
-                    </svg><!-- <i class="fas fa-chevron-down"></i> Font Awesome fontawesome.com -->
-                    <ul class="nav-global__child">
-                        <li><a href="https://jkt48.com/theater?lang=id">Apa Itu Teater JKT48</a></li>
-                        <li><a href="https://jkt48.com/360tour">Theater Virtual Tour 360<sup>o</sup></a></li>
-                        <li><a href="https://jkt48.com/about/ticketing?lang=id">Tata cara pembelian ticket</a></li>
-                        <li><a href="https://jkt48.com/theater/schedule?lang=id">Jadwal Pertunjukan + Pembelian Ticket</a></li>
-                        <li><a href="https://jkt48.com/event/schedule?lang=id">Jadwal Event + Pembelian Ticket</a></li>
-                    </ul>
-                </li>
-                <li class="nav-global__item">JKT48 POINTS <svg class="svg-inline--fa fa-chevron-down fa-w-14" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="chevron-down" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" data-fa-i2svg="">
-                        <path fill="currentColor" d="M207.029 381.476L12.686 187.132c-9.373-9.373-9.373-24.569 0-33.941l22.667-22.667c9.357-9.357 24.522-9.375 33.901-.04L224 284.505l154.745-154.021c9.379-9.335 24.544-9.317 33.901.04l22.667 22.667c9.373 9.373 9.373 24.569 0 33.941L240.971 381.476c-9.373 9.372-24.569 9.372-33.942 0z"></path>
-                    </svg><!-- <i class="fas fa-chevron-down"></i> Font Awesome fontawesome.com -->
-                    <ul class="nav-global__child">
-                        <li><a href="https://jkt48.com/about/jktpoints?lang=id">Cara Beli &amp; Pakai JKT48 Points</a></li>
-                        <li><a href="https://jkt48.com/jktpoints?lang=id">Beli JKT48 Points</a></li>
-                    </ul>
-                </li>
-                <li class="nav-global__item">SHOP <svg class="svg-inline--fa fa-chevron-down fa-w-14" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="chevron-down" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" data-fa-i2svg="">
-                        <path fill="currentColor" d="M207.029 381.476L12.686 187.132c-9.373-9.373-9.373-24.569 0-33.941l22.667-22.667c9.357-9.357 24.522-9.375 33.901-.04L224 284.505l154.745-154.021c9.379-9.335 24.544-9.317 33.901.04l22.667 22.667c9.373 9.373 9.373 24.569 0 33.941L240.971 381.476c-9.373 9.372-24.569 9.372-33.942 0z"></path>
-                    </svg><!-- <i class="fas fa-chevron-down"></i> Font Awesome fontawesome.com -->
-                    <ul class="nav-global__child">
-                        <li><a href="https://jkt48.com/shopping/form?lang=id">JKT48 Web Shop</a></li>
-                        <li><a href="https://www.tokopedia.com/officialjkt48" target="_blank">JKT48 Shop at Tokopedia</a></li>
-                        <li><a href="https://shopee.co.id/officialjkt48" target="_blank">JKT48 Shop at Shopee</a></li>
-                    </ul>
-                </li>
-                <li class="nav-global__item"><a href="https://jkt48.com/fanclub?lang=id">FANCLUB</a></li>
+                <li class="nav-global__item"><a href="<?= base_url('portal'); ?>">NEWS</a></li>
+                <li class="nav-global__item"><a href="#">CLIENT</a></li>
+                <li class="nav-global__item"><a href="#">MEMBER</a></li>
+                <li class="nav-global__item"><a href="#">LOCATION</a></li>
             </ul>
         </div>
     </nav>
@@ -81,7 +70,7 @@
         <div class="nav-breadcrumb">
             <ol class="nav-breadcrumb__list" itemscope="" itemtype="http://schema.org/BreadcrumbList">
                 <li class="nav-breadcrumb__item" itemprop="itemListElement" itemscope="" itemtype="http://schema.org/ListItem">
-                    <a itemtype="http://schema.org/Thing" itemprop="item" href="https://jkt48.com/">
+                    <a itemtype="http://schema.org/Thing" itemprop="item" href="#">
                         <span itemprop="name"><svg class="svg-inline--fa fa-home fa-w-18" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="home" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" data-fa-i2svg="">
                                 <path fill="currentColor" d="M280.37 148.26L96 300.11V464a16 16 0 0 0 16 16l112.06-.29a16 16 0 0 0 15.92-16V368a16 16 0 0 1 16-16h64a16 16 0 0 1 16 16v95.64a16 16 0 0 0 16 16.05L464 480a16 16 0 0 0 16-16V300L295.67 148.26a12.19 12.19 0 0 0-15.3 0zM571.6 251.47L488 182.56V44.05a12 12 0 0 0-12-12h-56a12 12 0 0 0-12 12v72.61L318.47 43a48 48 0 0 0-61 0L4.34 251.47a12 12 0 0 0-1.6 16.9l25.5 31A12 12 0 0 0 45.15 301l235.22-193.74a12.19 12.19 0 0 1 15.3 0L530.9 301a12 12 0 0 0 16.9-1.6l25.5-31a12 12 0 0 0-1.7-16.93z"></path>
                             </svg><!-- <i class="fas fa-home"></i> Font Awesome fontawesome.com --> Home</span>
